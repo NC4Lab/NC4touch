0d0febc6a0ffaf1488bb4f2c815fb96444bfc001
Manu Madhav <manusmad@gmail.com>
2024-10-28 15:20:56 -0700

    Adding Buzzer code

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,20 @@
 # On a Raspberry Pi 4, create initial functions for the project
 
 import smbus
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(27, GPIO.OUT)
 
 I2C_CHANNEL = 1
+
+def activate_buzzer():
+    GPIO.output(27, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(27, GPIO.LOW)
+    
 
 
 # Scan I2C bus for devices
@@ -19,3 +31,5 @@ def scan_i2c():
 
 if __name__ == '__main__':
     print(scan_i2c())
+
+    activate_buzzer
