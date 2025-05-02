30f36a77c620893ed312f5d107eccddaa91bdf5b
Manu S. Madhav <manu.madhav@ubc.ca>
2025-05-02 11:03:58 -0700

    Camera streaming and recording now fully working using ustreamer and ffmpeg

--- Controller/Camera.py
+++ Controller/Camera.py
@@ -1,96 +1,92 @@
-import cv2
 import os
-from video_recorder import VideoRecorder
 import subprocess
 import netifaces
-import logging
 
 class Camera:
-    def __init__(self, camera_device="/dev/video0"):
+    def __init__(self, camera_device="/dev/video0", stream_port=8080):
         self.camera_device = camera_device
 
         self.network_stream = None
-
-        self.video_capture = None
+        self.stream_port = stream_port
         self.video_recorder = None
 
-        self.frame_width = 640
-        self.frame_height = 480
-        self.fps = 30
-        self.video_codec = cv2.VideoWriter_fourcc(*'X264')
+        # Kill any existing ustreamer or ffmpeg processes
+        self.kill_ustreamer()
+        self.kill_ffmpeg()
 
         # Start the video capture
-        self.initialize_video_capture()
+        self.start_video_stream()
     
     def __del__(self):
         # Release resources
-        self.release_resources()
+        self.stop_video_stream()
         print("Camera object deleted.")
     
-    def initialize_video_capture(self):
-        self.video_capture = cv2.VideoCapture(0)
-        if not self.video_capture.isOpened():
-            print(f"Warning: Could not open camera ({self.camera_device}) with V4L2.")
-            return
-        
-        self.video_capture.set(cv2.CAP_PROP_FOURCC, self.video_codec)
-        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
-        self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
-        self.video_capture.set(cv2.CAP_PROP_FPS, self.fps)
-
-    def initialize_network_stream(self, port=8080):
-        # Use ustreamer
-        # cmd = ustreamer --device=/dev/video0 --host=<IP> --port <port>
+    def kill_ustreamer(self):
+        # Kill all ustreamer processes
+        try:
+            subprocess.call(['pkill', '-f', 'ustreamer'])
+            print("Killed all ustreamer processes.")
+        except Exception as e:
+            print(f"Error killing ustreamer processes: {e}")
+    
+    def kill_ffmpeg(self):
+        # Kill all ffmpeg processes
+        try:
+            subprocess.call(['pkill', '-f', 'ffmpeg'])
+            print("Killed all ffmpeg processes.")
+        except Exception as e:
+            print(f"Error killing ffmpeg processes: {e}")
+    
+    def start_video_stream(self):
+        # Use ustreamer to stream video
         # Get the local IP address
         ip = netifaces.ifaddresses('eth0')
         local_ip = ip[netifaces.AF_INET][0]['addr']
 
         print(f"Local IP address: {local_ip}")
-        cmd = f"ustreamer --device={self.camera_device} --host={local_ip} --port={port}"
+        cmd = f"ustreamer --device={self.camera_device} --host={local_ip} --port={self.stream_port} --sink=demo::ustreamer::sink --sink-mode=660 --sink-rm"
         print(f"Starting ustreamer with command: {cmd}")
         
         # Start the ustreamer process in the background
-        self.network_stream = subprocess.Popen(cmd)
+        self.network_stream = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
         
         if self.network_stream:
-            print(f"Network stream started on {local_ip}:{port}")
+            print(f"Network stream started on {local_ip}:8080")
         else:
             print("Failed to start network stream.")
     
-    def start_recording(self, output_file="output.mp4"):
+    def stop_video_stream(self):
+        self.stop_recording()
+        if self.network_stream:
+            # self.network_stream.kill()
+            os.killpg(os.getpgid(self.network_stream.pid), subprocess.signal.SIGTERM)
+            self.network_stream = None
+            print("Network stream terminated.")
+        else:
+            print("No network stream to terminate.")
+    
+    def start_recording(self, output_file="/mnt/shared/output.mp4"):
         if not self.video_recorder:
-            self.video_recorder = VideoRecorder(self.video_capture)
-            self.video_recorder.start_recording(output_file)
+            cmd = f"ustreamer-dump --sink=demo::ustreamer::sink --output - | ffmpeg -use_wallclock_as_timestamps 1 -i pipe: -c:v libx264 {output_file}"
+            print(f"Starting recording with command: {cmd}")
+            # Start the video recorder
+            self.video_recorder = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
             print(f"Recording started: {output_file}")
         else:
             print("Already recording.")
     
     def stop_recording(self):
         if self.video_recorder:
-            self.video_recorder.stop_recording()
+            # Stop the video recorder
+            os.killpg(os.getpgid(self.video_recorder.pid), subprocess.signal.SIGTERM)
             self.video_recorder = None
             print("Recording stopped.")
         else:
             print("No recording in progress.")
 
-    def release_resources(self):
-        if self.video_capture:
-            self.video_capture.release()
-            self.video_capture = None
-            print("Video capture released.")
-        if self.network_stream:
-            self.network_stream.terminate()
-            self.network_stream = None
-            print("Network stream terminated.")
-        if self.video_recorder:
-            self.video_recorder.terminate_ffmpeg_process()
-            self.video_recorder = None
-            print("Video recorder terminated.")
-
-
 if __name__ == "__main__":
     camera = Camera()
-    camera.initialize_network_stream()
 
     while True:
         pass
