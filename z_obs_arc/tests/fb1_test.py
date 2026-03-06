c8ec36b402155f4c67b0ee1ce5ea06b48cbf45dd
Matthew Cooke <mattcooke@live.com>
2026-03-05 20:19:59 -0800

    docs: Delete `VIRTUAL_CHAMBER_GUIDE.md` and `README_4_GALE.md`.

--- z_obs_arc/tests/fb1_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import time
-
-# Open the framebuffer for /dev/fb1
-buf = np.memmap('/dev/fb1', dtype='uint16', mode='w+', shape=(480, 320))  # Adjusted for driver-configured resolution
-buf[:] = 0xFFFF  # Fill with white
-time.sleep(1)
-buf[:] = 0x0000  # Clear to black
