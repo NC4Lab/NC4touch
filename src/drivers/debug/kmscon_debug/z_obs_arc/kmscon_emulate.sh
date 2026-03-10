16fdd213505edf03dbf355572e705745550619e3
Matthew Cooke <mattcooke@live.com>
2026-03-10 15:21:44 -0700

    Remove obsolete device tree overlay, test script, and peripheral classes
    - Deleted the device tree overlay for MPS3520 as it is no longer needed.
    - Removed the test script for framebuffer manipulation.
    - Eliminated the BeamBreak, Buzzer, LCD, LED, and Reward classes from the peripherals module as they are deprecated.

--- src/drivers/debug/kmscon_debug/z_obs_arc/kmscon_emulate.sh
+++
@@ -1,4 +0,0 @@
-modetest -M nc4_ili9488 -s 31@34:320x480
-modetest -M nc4_ili9488 -P 32@34:320x480
-sudo fbi -d /dev/fb0 -T 1 /home/nc4/TouchscreenApparatus/data/images/A01.bmp
-sudo fbi -d /dev/fb1 -T 1 /home/nc4/TouchscreenApparatus/data/images/B01.bmp
